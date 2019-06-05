c648f70cf84d1e48cee4f0eefd2c2a380b9fa2eb
Andrea Maugars <andrea.maugars@gmail.com>
2019-06-05 16:36:16 +0300

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
 import ListPage from './components/ListPage';
 import CreatePage from './components/CreatePage';
 
 const App = () => (
     <div className="App">
-        <ListPage />
-        <CreatePage />
+        <ul>
+            <li>
+                <Link to="/view">View rooms</Link>
+            </li>
+            <li>
+                <Link to="/create">Create room</Link>
+            </li>
+        </ul>
+        <Route path="/view" component={ListPage} />
+        <Route path="/create" component={CreatePage} />
     </div>
 );
 
