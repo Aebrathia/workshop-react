17e00040edbd614e4b867035aa429dcecf73f5fa
Andrea Maugars <andrea.maugars@gmail.com>
2019-06-19 17:36:09 +0300

    Finish edit page

--- src/components/EditPage.js
+++ src/components/EditPage.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 
 class EditPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: null,
-            number: '',
-            type: '',
+            shouldRedirect: false,
+            room: {
+                id: null,
+                number: '',
+                type: '',
+            },
         };
     }
 
@@ -16,19 +20,30 @@ class EditPage extends Component {
         const { id } = match.params;
         const response = await axios.get(`/rooms/${id}`);
         const { number, type } = response.data;
-        this.setState({ id, number, type });
+        this.setState({ room: { id: Number(id), number, type } });
     }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+        const { id } = this.state.room;
+        await axios.put(`/rooms/${id}`, this.state.room);
+        this.setState({ shouldRedirect: true });
+    };
 
     handleChange = event => {
         const { name, value } = event.target;
-        this.setState({ [name]: value });
+        this.setState(prevState => ({ room: { ...prevState.room, [name]: value } }));
     };
 
     render() {
-        const { number, type } = this.state;
+        const {
+            room: { number, type },
+            shouldRedirect,
+        } = this.state;
         return (
             <div className="EditPage">
-                <form onSubmit={() => {}}>
+                {shouldRedirect && <Redirect to="/view" />}
+                <form onSubmit={this.handleSubmit}>
                     <label htmlFor="number">Number</label>
                     <input
                         id="number"
@@ -46,7 +61,7 @@ class EditPage extends Component {
                         value={type}
                         onChange={this.handleChange}
                     />
-                    <button type="submit">Create new room</button>
+                    <button type="submit">Edit room</button>
                 </form>
             </div>
         );
